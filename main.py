0f1303856ae90d1bd4072fca58cfe669763495af
Alvaro Zamora <alvarozamora@stanford.edu>
2021-09-10 07:45:44 -0700

    Minimum Viable Product

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-import yfinance
+import yfinance as yf
 import numpy as np
+from utils import *
 
-tickers = np.genfromtxt('watchlist.txt')
-print(tickers)
+# Read in tickers from watchlist.txt
+tickers = np.genfromtxt('watchlist.txt', dtype=str)
+
+# Read in database
+db = load_database()
+
+# Gather option chain data
+for ticker in tickers:
+    
+    yticker = yf.Ticker(ticker)
+
+    ticker_dates = yticker.options
+    if len(ticker_dates) == 0:
+        print(f"{ticker.upper()} has no options.")
+    else:
+        if ticker not in db.keys():
+            db[ticker] = []
+        for date in yticker.options:
+            if db is not None and date not in db[ticker]:
+                print(f'New option expiry date found for {ticker}: {date}')
+                db[ticker].append(date)
+
+        save_database(db)
--- utils.py
+++ utils.py
@@ -1,14 +1,24 @@
 import pickle
+import glob
 
 def save_obj(obj, name):
-    with open('obj/'+ name + '.pkl', 'wb') as f:
+    with open('db/'+ name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name):
-    with open('obj/' + name + '.pkl', 'rb') as f:
+    with open('db/' + name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
 
-def redundant_save(obj, name, redunancy=4):
-    for i in range(redunancy):
-        save_obj(obj,f'db{i+1}')
+def load_database():
+
+    if len(glob.glob('db/*')) == 0:
+        print("No Database Found.")
+        return {}
+    
+    else:
+        db = load_obj('db')
+        return db
+
+def save_database(db):
+    save_obj(db,'db')
